207c49c4ccc7277369bd8756c4a0951888c86f8c
Emmanuel <emma145@hotmail.es>
2022-03-24 13:21:30 -0500

    working on implementing observables

--- src/app/services/language.service.ts
+++ src/app/services/language.service.ts
@@ -1,31 +1,33 @@
 import { Injectable } from "@angular/core";
-import * as english from '../../assets/languages/english.json';
-import * as spanish from '../../assets/languages/spanish.json';
+import { Subject } from "rxjs";
+import * as english_json from '../../assets/languages/english.json';
+import * as spanish_json from '../../assets/languages/spanish.json';
 
 @Injectable()
 export class LanguageService {
-    public text: any;
     public language: string;
+
+    public subject_language: Subject<any>;
+    public message_language: any;
 
     constructor() {
         this.language = "english";
+
+        this.subject_language = new Subject();
+        this.message_language = this.subject_language.asObservable();
     }
 
     getText(): any {
-        switch (this.language) {
-            case 'english':
-                return english;
-
-            case 'spanish':
-                return spanish;
-
-            default:
-                console.log(this.language)
-                return english;
-        }
+        /* this.subject_language.next() */
     }
 
     setLanguage(language: string): void {
-        this.language = language;
+        if (language == 'english') {
+            this.message_language = english_json;
+        } else if ( language == 'spanish') {
+            this.message_language = spanish_json;
+        } else {
+            this.message_language = english_json;
+        }
     }
 }
