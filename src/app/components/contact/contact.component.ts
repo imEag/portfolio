8808d46ecb1c7e0a889eb6fd12c2d9aa8dd412f3
Emmanuel <emma145@hotmail.es>
2022-05-28 13:42:38 -0500

    add languae service to contact

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { LanguageService } from 'src/app/services/language.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  //Store subscription to LAnguage service
+  public subscription!: Subscription;
+
+  //Stores text from language service
+  public text: any;
+
+  constructor(
+    private _LanguageService: LanguageService
+  ) { }
 
   ngOnInit(): void {
+    //gets default text 
+    this.text = this._LanguageService.getTextDefault();
+
+    //Loads any language change from language service.
+    this.subscription = this._LanguageService.message_language.subscribe((text: any) => {
+      this.text = text;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe;
   }
 
 }
